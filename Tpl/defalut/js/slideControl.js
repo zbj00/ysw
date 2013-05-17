2bf0ae62368371b4df0bcc4ed52d3926e5f3b5ad
zbj00 <wangqiang789@outlook.com>
2013-05-18 00:32:12 +0800

    增加幻灯展示点击到搜索页面功能
    Signed-off-by: zbj00 <wangqiang789@outlook.com>

---
+++ Tpl/defalut/js/slideControl.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: true
+  });
+  
+  
+  $(".slide").click(function(e){
+	var wd = $(this).attr("data-wd");
+	location.href = "index.php?s=vod-search-wd-" + wd;
+  });
+  
+  
+});
